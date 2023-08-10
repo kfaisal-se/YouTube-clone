3f9821dfbd6abeeef60b883df1b84741d0ff3ab6
Faaaiiisssaaal <faisal@Faisals-MacBook-Air.local>
2023-08-10 18:21:09 +0530

    Updated API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // const KEY = "AIzaSyCaavVEmT6T59AwEIaaMBvlqxn2_Fhg9gw";
 const KEY = import.meta.env.API_KEY;
+console.log( "key -->> ", KEY);
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
